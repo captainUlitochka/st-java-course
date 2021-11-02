0053d8b465328a0718f6b41bd7745779428ea889
captainUlitochka <88254738+captainUlitochka@users.noreply.github.com>
2021-11-02 23:20:02 +0300

    Fluent-интерфейсы для тестов групп по лекции 5.4

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -1,34 +1,15 @@
 package ru.stqa.pft.addressbook.model;
 
 public class GroupData {
-  public void setId(int id) {
-    this.id = id;
-  }
 
-  private int id;
-  private final String groupName;
-  private final String groupHeader;
-  private final String groupFooter;
+  private int id = Integer.MAX_VALUE;;
+
+  private String groupName;
+  private String groupHeader;
+  private String groupFooter;
 
   public int getId() {
     return id;
-  }
-
-  public GroupData(int id, String groupName, String groupHeader, String groupFooter) {
-
-    this.id = id;
-    this.groupName = groupName;
-    this.groupHeader = groupHeader;
-    this.groupFooter = groupFooter;
-  }
-
-
-  public GroupData(String groupName, String groupHeader, String groupFooter) {
-
-    this.id = Integer.MAX_VALUE;
-    this.groupName = groupName;
-    this.groupHeader = groupHeader;
-    this.groupFooter = groupFooter;
   }
 
   public String getGroupName() {
@@ -43,6 +24,24 @@ public class GroupData {
     return groupFooter;
   }
 
+  public GroupData withId(int id) {
+    this.id = id;
+    return this;
+  }
+  public GroupData withName(String groupName) {
+    this.groupName = groupName;
+    return this;
+  }
+
+  public GroupData withHeader(String groupHeader) {
+    this.groupHeader = groupHeader;
+    return this;
+  }
+
+  public GroupData withFooter(String groupFooter) {
+    this.groupFooter = groupFooter;
+    return this;
+  }
 
   @Override
   public String toString() {
